8b361ead2002cab57844365cedf1cba65a603309
Sebastian Vetter <sebastian@roadside-developer.com>
2013-09-23 13:03:30 +1000

    Add models for m2m and json apps

--- cpoc/generic_m2m/models.py
+++ cpoc/generic_m2m/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+from genericm2m.models import RelatedObjectsDescriptor
+
+
+class Container(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+
+    blocks = RelatedObjectsDescriptor()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Container, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return "Container {}".format(self.name)
+
+
+class ContentBlock(models.Model):
+    display_order = models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        if self.display_order is None:
+            self.display_order = 0
+        super(ContentBlock, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return "Content block {}".format(self.display_order)
+
+    class Meta:
+        abstract = True
+
+
+class TextBlock(ContentBlock):
+    text = models.TextField()
+
+    def __unicode__(self):
+        return "Textblock '{}'".format(self.text[:20])
+
+
+class ImageBlock(ContentBlock):
+    image = models.ImageField(upload_to="images")
+
+    def __unicode__(self):
+        return "Image block '{}'".format(self.image.path)
+
+
+class UserBlock(ContentBlock):
+    user = models.ForeignKey('auth.User', related_name="+")
+
+    def __unicode__(self):
+        return "User block for {}".format(self.user.username)
+
+
+for idx in xrange(0, 20):
+    class Meta:
+        app_label = 'generic_m2m'
+    attrs = {
+        'text': models.TextField(),
+        '__module__': 'cpoc.generic_m2m',
+        'Meta': Meta,
+    }
+    type("Test{}Block".format(idx), (ContentBlock,), attrs)
--- cpoc/json_data/models.py
+++ cpoc/json_data/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.datastructures import SortedDict
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+from jsonfield import JSONField
+
+
+class Container(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+
+    blocks = JSONField(load_kwargs={'object_pairs_hook': SortedDict})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        if self.blocks is None:
+            self.blocks = []
+        super(Container, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return "Container {}".format(self.name)
