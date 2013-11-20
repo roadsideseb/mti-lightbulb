637f7476c3ae37e1d7a75b0917796718afcf9806
Sebastian Vetter <sebastian@roadside-developer.com>
2013-11-20 21:31:50 +1100

    Use nvd3 to render graphs nicer

--- visualize/app.py
+++ visualize/app.py
@@ -3,10 +3,14 @@ import os
 from collections import defaultdict
 
 from flask import Flask
+from flask import json
 from flask import request
 from flask import render_template
 
 app = Flask(__name__)
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+RESULTS_DIR = os.path.join(PROJECT_DIR, '../results')
 
 
 @app.route('/result')
@@ -16,11 +20,9 @@ def visualise_file():
         'postgresql', 'mysql', 'sqlite']
     app_names = request.args.getlist('app_name') or [
         'polymorphic_test', 'model_utils_test', 'generic_m2m']
-    metric = request.args.get('metric', 'query_time_sql')
 
     fn_tmpl = '{app_name}_{db_vendor}_Django-{version}_benchmark_results.csv'
 
-    base_dir = os.path.join(os.getcwd(), '../results')
     grouped_data = defaultdict(list)
     for app_name in app_names:
         for version in versions:
@@ -28,25 +30,23 @@ def visualise_file():
                 name = '{}_{}_{}'.format(app_name, version, db_vendor)
                 filename = fn_tmpl.format(
                     app_name=app_name, db_vendor=db_vendor, version=version)
-                path = os.path.join(base_dir, filename)
+                path = os.path.join(RESULTS_DIR, filename)
                 if not os.path.exists(path):
                     continue
                 with open(path) as fh:
                     for line in fh:
-                        grouped_data[name].append(line)
-    return render_template('visualise_file.html', grouped_data=grouped_data,
-                           metric=metric)
+                        try:
+                            line = json.loads(line)
+                            grouped_data[name].append(line)
+                        except:
+                            pass
+    return json.dumps(grouped_data)
 
 
 @app.route('/')
 def index():
-    base_dir = os.path.join(os.getcwd(), '../results')
-    files = []
-    for filename in os.listdir(base_dir):
-        if filename.endswith('.csv'):
-            filename, __ = os.path.splitext(filename)
-            files.append(filename)
-    return render_template('index.html', filenames=files)
+    metric = request.args.get('metric', 'query_time_sql')
+    return render_template('visualise_file.html', metric=metric)
 
 
 if __name__ == '__main__':
